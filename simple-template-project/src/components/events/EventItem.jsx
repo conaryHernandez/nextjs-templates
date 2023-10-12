2ef1375ef30232c897d90a2e5ee710759900ddd6
conaryHernandez <conaryh@gmail.com>
2023-10-11 19:57:05 -0600

    simplete-template-project: adding event detail components

--- simple-template-project/src/components/events/EventItem.jsx
+++ simple-template-project/src/components/events/EventItem.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import Link from 'next/link';
 
 import classes from './EventItem.module.css';
+import DateIcon from '../icons/date-icon';
 import Button from '../atoms/Button';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 const EventItem = ({ event }) => {
   const humanReadableDate = new Date(event.date).toLocaleDateString('en-US', {
@@ -21,14 +23,21 @@ const EventItem = ({ event }) => {
         <div className={classes.summary}>
           <h2>{event.title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- simple-template-project/src/pages/events/[eventId].jsx
+++ simple-template-project/src/pages/events/[eventId].jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import EventSummary from '@/components/events/EventDetail/EventSummary';
+import EventLogistics from '@/components/events/EventDetail/EventLogistics';
+import EventContent from '@/components/events/EventDetail/EventContent';
+import { getEventById } from '../../../dummy-data';
 
 const EventDetails = () => {
-  return <div>EventDetails</div>;
+  const router = useRouter();
+
+  console.log(router);
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default EventDetails;
