e9608b32278743483c2da4aa015014287e4d2293
conaryHernandez <conaryh@gmail.com>
2023-10-16 12:02:48 -0600

    ssr-fetching-template: testing SSR

--- ssr-fetching-template/pages/index.js
+++ ssr-fetching-template/pages/index.js
@@ -1,11 +1,31 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  // cwd = current working directory / makes sure that we are in the right folder
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+
+  const jsonData = await fs.readFile(filePath, 'utf-8');
+
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
