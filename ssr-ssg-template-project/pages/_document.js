35de2c53caef532514928d98a3f25e8cea35373f
conaryHernandez <conaryh@gmail.com>
2023-10-26 10:47:27 -0600

    ssr-fetching-project-template: image config

--- ssr-ssg-template-project/pages/_document.js
+++ ssr-ssg-template-project/pages/_document.js
@@ -3,13 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <html lang='en'>
+      <Html lang='en'>
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
