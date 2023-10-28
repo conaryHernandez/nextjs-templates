7927728b03c7fd570a4c37e469d968293d54ca5f
conaryHernandez <conaryh@gmail.com>
2023-10-28 06:30:27 -0600

    api-routes: feedback route

--- api-routes-template/pages/index.js
+++ api-routes-template/pages/index.js
@@ -1,7 +1,35 @@
 function HomePage() {
+  const feedbackHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = event.target.email.value;
+    const enteredFeedback = event.target.feedback.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+
+      <form onSubmit={feedbackHandler}>
+        <label htmlFor='email'>Your Email Address</label>
+        <input type='text' name='email' />
+
+        <label htmlFor='feedback'>Your Feedback</label>
+        <textarea name='feedback' rows='5'></textarea>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
