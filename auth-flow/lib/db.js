eeb19f74370b24196d5136eb0f45a90cbb90311f
conaryHernandez <conaryh@gmail.com>
2023-11-23 12:57:45 -0600

    auth-flow: add db connection and signup logic

---
+++ auth-flow/lib/db.js
@@ -0,0 +1,11 @@
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export async function connectDatabase() {
+  await client.connect();
+  return client;
+}
